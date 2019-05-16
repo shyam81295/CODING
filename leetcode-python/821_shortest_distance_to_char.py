5377c9810501a4374cc6ce3dda1cce7040b9021b
Shyam Singh <shyam81295@gmail.com>
2019-05-16 23:40:01 +0530

    O(N) solution added

--- leetcode-python/821_shortest_distance_to_char.py
+++ leetcode-python/821_shortest_distance_to_char.py
@@ -14,3 +14,26 @@ class Solution:
         return ans_list
         
 # O(N) solution
+# calculate distance from immediate left 'C' for each element in O(N) loop
+# then from immediate right 'C' for each element in O(N) loop
+# for left, initialise to float('-inf') and for right, initialise to float('inf')
+class Solution:
+    def shortestToChar(self, S: str, C: str) -> List[int]:
+        ans_list = [0]*len(S)
+        
+        prev = float('-inf')
+        for i in range(len(S)):
+            if S[i] == C:
+                prev = i
+                ans_list[i] = 0
+            else:
+                ans_list[i] = i-prev
+                
+        next = float('inf')
+        for i in range(len(S)-1, -1 , -1):
+            if S[i] == C:
+                ans_list[i] = 0
+                next = i
+            else:
+                ans_list[i] = min(ans_list[i],next-i)
+        return ans_list
