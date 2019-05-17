6608e06cb7958bcc53c92c27b9bb9193bf601970
Shyam Singh <shyam81295@gmail.com>
2019-05-18 03:14:39 +0530

    Update EASY_1-two_sum.py

--- leetcode-python/EASY_1-two_sum.py
+++ leetcode-python/EASY_1-two_sum.py
@@ -23,3 +23,15 @@ class Solution:
                     
         return ans_list
         
+# O(n) solution
+# use dict
+# if target-num[i] is present, then return its index.
+# else assign its index to dict.
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ans_dict = {}
+        for i in range(len(nums)):
+            if target-nums[i] in ans_dict:
+                return [ans_dict[target-nums[i]], i]
+            else:
+                ans_dict[nums[i]] = i
